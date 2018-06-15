167f65ebe3f4face88ffaba5ed9359dbf52ed7ad
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2018-06-15 13:45:18 +0200

    setup

---
+++ src/main/java/nl/adben/reactiveofficers/ReactiveOfficersApplication.java
@@ -0,0 +1,12 @@
+package nl.adben.reactiveofficers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveOfficersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveOfficersApplication.class, args);
+    }
+}
