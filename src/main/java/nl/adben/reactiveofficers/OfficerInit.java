9df0050642246e03055b5c7e7102b348e00b7fdc
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2018-06-15 14:39:24 +0200

    added reactive controller, data init

---
+++ src/main/java/nl/adben/reactiveofficers/OfficerInit.java
@@ -0,0 +1,31 @@
+package nl.adben.reactiveofficers;
+
+import nl.adben.reactiveofficers.dao.OfficerRepository;
+import nl.adben.reactiveofficers.entities.Officer;
+import nl.adben.reactiveofficers.entities.Rank;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Component
+public class OfficerInit implements ApplicationRunner {
+    private OfficerRepository dao;
+
+    public OfficerInit(OfficerRepository dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        dao.deleteAll()
+                .thenMany(Flux.just(new Officer(Rank.CAPTAIN, "James", "Kirk"),
+                        new Officer(Rank.CAPTAIN, "Jean-Luc", "Picard"),
+                        new Officer(Rank.CAPTAIN, "Benjamin", "Sisko"),
+                        new Officer(Rank.CAPTAIN, "Kathryn", "Janeway"),
+                        new Officer(Rank.CAPTAIN, "Jonathan", "Archer")))
+                .flatMap(dao::save)
+                .thenMany(dao.findAll())
+                .subscribe(System.out::println);
+    }
+}
